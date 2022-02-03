ed50c382118762196bc75c6ec5c821d6edfc501e
AbidarYassine <yassine.abidar@edu.uca.ma>
2022-02-03 03:03:21 +0100

    add collections detail && generate QR

---
+++ src/components/Collection/QrCode.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import { Button, Card, CardContent, Container, Grid, TextField } from "@mui/material";
+import "./collectionDetail.css";
+import qrcode from "qrcode";
+
+const QRCode = ({ id_collection, libelle, videos }) => {
+  const [text, setText] = useState(id_collection || "");
+  const [imageUrl, setImageUrl] = useState("");
+  const getUrlsVideos = () => {
+    const jsonString = JSON.stringify(videos);
+    return jsonString;
+  };
+  const generateQrCode = async () => {
+    try {
+      const response = await qrcode.toDataURL(getUrlsVideos());
+      console.log("res ", response);
+      setImageUrl(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <>
+      <Container className="container">
+        <Card>
+          <h2 className="title">Generate Download QR For collection of {libelle || "Default name"}</h2>
+          <CardContent>
+            <Grid container spacing={2}>
+              <Grid className="grid-center" item xl={4} lg={4} md={6} sm={12} xs={12}>
+                <Button className="btn" variant="contained"
+                        color="primary" onClick={() => generateQrCode()}>Generate</Button>
+                <div className="qr-image-container">
+                  {imageUrl ? (
+                    <a href={imageUrl} download>
+                      <img title="Download the QR Code" src={imageUrl} alt="img" />
+                    </a>) : null}
+                </div>
+              </Grid>
+            </Grid>
+          </CardContent>
+        </Card>
+      </Container>
+    </>
+  );
+};
+
+export default QRCode;
